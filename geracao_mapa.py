25a34826e4de8140ea6491b7b097fc392b11e8c1
IgorPrajnaDev <igor.melo@academico.ufpb.br>
2024-10-01 15:50:35 -0300

    modulo geracao_mapa.py com 1 função pra criar a matriz. Argumentos: numero de linhas, numero de buracos e numero de tesouros

--- geracao_mapa.py
+++ geracao_mapa.py
@@ -1,49 +1,50 @@
 import random
 
-linhas = 4
-#criação da matriz
-celula = [[None for i in range (linhas)] for j in range (linhas)]
+def geracao_matriz (linhas, n_buracos, n_tesouros ):
 
-#atribuição de buracos ("B") 
+    #criação da matriz
 
-buracos = 0
-while buracos < 6:
-    i = random.randint(0, linhas-1)
-    j = random.randint(0, linhas-1)
+    celula = [[None for i in range (linhas)] for j in range (linhas)]
 
-    if celula[i][j] == "B":
-        continue
-    if celula[i][j] is None:
-        celula[i][j] = "B"
-        buracos += 1
+    #atribuição de buracos ("B") 
 
+    buracos = 0
+    while buracos < n_buracos:
+        i = random.randint(0, linhas-1)
+        j = random.randint(0, linhas-1)
 
-#Criação tesouros
-tesouros = 0
-while tesouros < 3:
-    i = random.randint(0, linhas-1)
-    j = random.randint(0,linhas-1)
-
-    if celula[i][j] == "B" or celula[i][j] == "T":
-        continue
-    else: celula[i][j] = "T"
-    tesouros += 1
-    
-    #colocar número de tesouros ao redor da célula
-
-for i in range (linhas):
-    for j in range (linhas):
+        if celula[i][j] == "B":
+            continue
         if celula[i][j] is None:
-            buracos_proximos = 0
-            if i-1 >= linhas and celula[i-1][j] == "B":
-                buracos_proximos += 1
-            if j-1 >= linhas and celula [i][j-1] == "B":
-                buracos_proximos += 1
-            if i+1 < linhas and celula[i+1][j] == "B":
-                buracos_proximos += 1
-            if j+1 < linhas and celula[i][j+1] == "B":
-                buracos_proximos += 1
-            celula[i][j] = buracos_proximos
+            celula[i][j] = "B"
+            buracos += 1
 
-for linha in celula:
-    print(linha)
+
+    #Criação tesouros
+    tesouros = 0
+    while tesouros < n_tesouros:
+        i = random.randint(0, linhas-1)
+        j = random.randint(0,linhas-1)
+
+        if celula[i][j] == "B" or celula[i][j] == "T":
+            continue
+        else: celula[i][j] = "T"
+        tesouros += 1
+        
+        #colocar número de tesouros ao redor da célula
+
+    for i in range (linhas):
+        for j in range (linhas):
+            if celula[i][j] is None:
+                buracos_proximos = 0
+                if i-1 >= linhas and celula[i-1][j] == "B":
+                    buracos_proximos += 1
+                if j-1 >= linhas and celula [i][j-1] == "B":
+                    buracos_proximos += 1
+                if i+1 < linhas and celula[i+1][j] == "B":
+                    buracos_proximos += 1
+                if j+1 < linhas and celula[i][j+1] == "B":
+                    buracos_proximos += 1
+                celula[i][j] = buracos_proximos
+    return(celula)
+
